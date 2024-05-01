24cfad8966ec9e8e55fe3a0dc5ebf4e37b8296d2
Antonio Carlos Virissimo <antonio13050@gmail.com>
2024-04-30 22:30:45 -0300

    feat: Created api-notification

---
+++ api-notification/src/main/java/com/example/apinotification/ApiNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.example.apinotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNotificationApplication.class, args);
+	}
+
+}
---
+++ api-notification/src/test/java/com/example/apinotification/ApiNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apinotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
